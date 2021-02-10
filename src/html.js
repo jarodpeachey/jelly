6db4613d737081742f237c6f71138ea225d0345c
Jarod Peachey <jwpeachey107@aol.com>
2021-02-10 07:38:11 -0500

    Fixes

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         {props.headComponents}
         <script async src="https://cdn.splitbee.io/sb.js"></script>
+        <script async defer data-domain="jellydevelopment.com" src="https://plausible.io/js/plausible.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
