ee7b549043151b7fe10a45cd639e9583f00fffaf
Jarod Peachey <jwpeachey107@aol.com>
2021-09-09 16:16:09 -0400

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,8 +66,7 @@ module.exports = {
         mergeScriptHashes: false,
         mergeStyleHashes: false,
         directives: {
-          "script-src": "'self' 'unsafe-inline' data: plausible.io cdn.splitbee.io",
-          "script-src": "'self' 'unsafe-inline' d33wubrfki0l68.cloudfront.net",
+          "script-src": "'self' 'unsafe-inline' data: plausible.io cdn.splitbee.io d33wubrfki0l68.cloudfront.net",
           "style-src": "'self' 'unsafe-inline'",
           // you can add your directives or override defaults
         },
