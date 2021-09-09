ddee0ec2b5dda1dd5102c1186f0aa207c4ca77d0
Jarod Peachey <jwpeachey107@aol.com>
2021-09-09 15:42:34 -0400

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,21 @@ module.exports = {
     },
     "gatsby-plugin-robots-txt",
     "gatsby-plugin-sitemap",
+        {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          "script-src": "'self' www.splitbee.io",
+          "script-src": "'self' www.plausible.io",
+          "style-src": "'self' 'unsafe-inline'",
+          // you can add your directives or override defaults
+        }
+      }
+    }
   ],
 };
