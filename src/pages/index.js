df366712f2218cd3a880647af4520780edd0669b
Jarod Peachey <jarodpeachey@gmail.com>
2023-08-14 11:28:42 -0400

    Remove extra period

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const Home = () => {
             <div className="row">
               <div className="col-md-10 col-lg-8 col-xl-6">
                 <h1>Websites that build trust and get more customers</h1>
-                <p className="section-description">Your website is your way of letting people know why they should trust your business..</p>
+                <p className="section-description">Your website is your way of letting people know why they should trust your business.</p>
                 <p className="section-description"> At Jelly Development, we focus on crafting a website that fits your business and turns visitors into customers</p>
 
                 <a href="/get-started" className="btn">
